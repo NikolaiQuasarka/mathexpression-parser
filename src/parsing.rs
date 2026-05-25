90fad4a70d9d93c35fed56b9a9d64dc846facb80
nikolai <nikolaiquasarka@gmail.com>
2026-05-25 21:27:37 +0300

    Added token types, from func and tests

--- src/parsing.rs
+++ src/parsing.rs
@@ -1,5 +1,26 @@
+use regex::Regex;
+
 pub struct ExpressionTokenizer {
     expression: String,
+}
+
+#[derive(Debug, PartialEq)]
+enum Token {
+    LeftBracket,
+    RightBracket,
+    Delimiter,
+    Operator(OperatorType),
+    Number(f64),
+    Other,
+}
+
+#[derive(Debug, PartialEq)]
+enum OperatorType {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
 }
 
 impl ExpressionTokenizer {
@@ -27,10 +48,31 @@ impl ExpressionTokenizer {
     }
 }
 
+impl Token {
+    fn from(str: &str) -> Self {
+        match str {
+            "(" => Self::LeftBracket,
+            ")" => Self::RightBracket,
+            "," => Self::Delimiter,
+            operator @ ("+" | "-" | "/" | "*" | "^") => Self::Operator(match operator {
+                "+" => OperatorType::Add,
+                "-" => OperatorType::Sub,
+                "/" => OperatorType::Div,
+                "*" => OperatorType::Mul,
+                "^" => OperatorType::Pow,
+                _ => unreachable!(),
+            }),
+
+            token @ _ => match token.parse::<f64>() {
+                Ok(number) => Self::Number(number),
+                Err(_) => Self::Other,
+            },
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use regex::Regex;
-
     use super::*;
 
     #[test]
@@ -41,5 +83,42 @@ mod tests {
         dbg!(&regex);
 
         assert!(regex.is_ok())
+    }
+
+    #[test]
+    fn token_from() {
+        let strings_and_expected_tokens = [
+            ("+", Token::Operator(OperatorType::Add)),
+            ("+", Token::Operator(OperatorType::Add)),
+            ("-", Token::Operator(OperatorType::Sub)),
+            ("/", Token::Operator(OperatorType::Div)),
+            ("*", Token::Operator(OperatorType::Mul)),
+            ("^", Token::Operator(OperatorType::Pow)),
+            (",", Token::Delimiter),
+            ("(", Token::LeftBracket),
+            (")", Token::RightBracket),
+        ];
+
+        for (string, token) in strings_and_expected_tokens {
+            assert_eq!(Token::from(string), token);
+        }
+
+        let string_expected_numbers = [
+            ("10", 10.0),
+            ("0.0934", 0.0934),
+            ("-32432.646", -32432.646),
+            ("325.", 325.0),
+            (".097996", 0.097996),
+        ];
+
+        for (string, token) in string_expected_numbers {
+            assert_eq!(Token::from(string), Token::Number(token))
+        }
+
+        let invalid_chars = ["f", "324h54", "12.0f"];
+
+        for string in invalid_chars {
+            assert_eq!(Token::from(string), Token::Other)
+        }
     }
 }
