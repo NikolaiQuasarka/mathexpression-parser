acfc4bae2c5bcf5ec9f6b15a88fc7d305f7a5ca1
nikolai <nikolaiquasarka@gmail.com>
2026-05-29 00:19:45 +0300

    Made real program

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,21 @@
+use std::{env::args, process::exit};
+
+use mathexpression_parser::calculator::calculate;
+
 fn main() {
+    app()
+}
+
+fn app() {
+    let input = args().skip(1).take(1).next().unwrap_or_else(|| {
+        eprintln!("Argument is missing");
+        exit(1)
+    });
+
+    match calculate(input) {
+        Ok(result) => {
+            println!("{result}")
+        }
+        Err(_) => eprintln!("Error occured"),
+    };
 }
